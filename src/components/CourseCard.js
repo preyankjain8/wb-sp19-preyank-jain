b0c23a14a707ef70309944665d36e0e3c4556633
preyank <preyank@ccs.neu.edu>
2019-01-31 23:26:42 -0500

    updated look and feel of course-card

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -7,10 +7,12 @@ const CourseCard = ({course, deleteCourse}) =>
     <img className="card-img-top"
          src="https://picsum.photos/300/200"/>
     <div className="card-body">
-      <h5 className="card-title">{course.title}</h5>
-      <p className="card-text">Card text.</p>
-      <Link className="btn btn-primary" to={`/course/${course.id}`}>Edit</Link>
-      <a onClick={() => deleteCourse(course)}
-         className="btn btn-danger">Delete</a>
+      <h5 className="card-title">
+        <Link to={`/course/${course.id}`}>{course.title}</Link>
+      </h5>
+      <p className="card-text">
+      <i className="fa fa-file-text" aria-hidden="true"></i> Modified 6:45 PM
+      <i onClick={ () => deleteCourse(course) } className="fa fa-trash float-right"></i>
+      </p>
     </div></div>
 export default CourseCard;
