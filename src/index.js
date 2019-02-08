5d067cf778b92f8480818f88fa1492ba294c0c01
preyank <preyank@ccs.neu.edu>
2019-02-07 21:19:59 -0500

    added assignment-4 (redux) starter code

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from 'react-dom'
 import HelloWorld from './components/hello'
 import '../node_modules/bootstrap/dist/css/bootstrap.css';
 import CourseManager from './containers/CourseManager'
+import WidgetList from './components/WidgetList'
+import WidgetListContainer from './containers/WidgetListContainer'
+import widgetReducer from './reducers/WidgetReducer'
+import {createStore} from 'redux'
+import {Provider} from 'react-redux'
+
+const store = createStore(widgetReducer);
 
 ReactDOM.render(
-  <div className="container-fluid">
+  <Provider store={store}>
     <CourseManager/>
-  </div>,
+  </Provider>,
   document.getElementById("root")
 );
